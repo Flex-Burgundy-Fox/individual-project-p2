269dac1e20cbbd5b7f0609ff1c327ff87074043a
bagusDhanist <bagusdhanist@gmail.com>
2021-09-10 13:40:17 +0700

    editing app.js

--- server/app.js
+++ server/app.js
@@ -3,14 +3,15 @@ if (process.env.NODE_ENV != "production") require("dotenv").config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const routes = require("./router/index.js")
 const cors = require("cors")
 const errHandler = require("./middleware/errHandler.js")
+const router = require("./router")
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(routes)
+
+app.use(router)
 app.use(errHandler)
 
 app.listen(port, () => {
